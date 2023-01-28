6d3217659c98d5be3a168669c6750ecb4365bea8
fujin <teodorp@protonmail.com>
2023-01-28 05:13:22 -0800

    new score system

--- assets/worlds/World.gd
+++ assets/worlds/World.gd
@@ -5,6 +5,7 @@ var elapsed_time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	MenuAudio.stop()
+	elapsed_time = 3
 	
 func _process(delta):
-	elapsed_time += 0.017
+	elapsed_time -= 0.017
